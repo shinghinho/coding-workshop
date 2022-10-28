999cf2083434e0c06df0ed888d70033b12d7afef
cst0313 <86826906+cst0313@users.noreply.github.com>
2022-10-28 09:00:04 +0800

    Update Jeffrey_calculator.cpp

--- project-1-calculator/Jeffrey_calculator.cpp
+++ project-1-calculator/Jeffrey_calculator.cpp
@@ -3,7 +3,7 @@ using namespace std;
 queue<string> q;
 set<char>op;
 set<char>num;
-bool lex(string x){
+bool lex(string x){ 
     string t="";
     for(int i=0;i<x.length();i++){
         if(x[i]==' '){
